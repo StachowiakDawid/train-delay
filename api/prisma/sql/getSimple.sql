ed21887601755c68d90b420819365bbb39716bcd
StachowiakDawid <dawidos234@gmail.com>
2025-10-27 18:03:42 +0100

    remove redundant

--- api/prisma/sql/getSimple.sql
+++ api/prisma/sql/getSimple.sql
@@ -1,15 +1,9 @@
-select s.station,
-            s.arrival_timestamp,
+select      s.arrival_timestamp,
             s.department_timestamp,
-            s.arrival_time,
-            s.department_time,
             s.arrival_delay,
             s.department_delay,
-            s2.station as s2_station,
             s2.arrival_timestamp as s2_arrival_timestamp,
             s2.department_timestamp as s2_department_timestamp,
-            s2.arrival_time as s2_arrival_time,
-            s2.department_time as s2_department_time,
             s2.arrival_delay as s2_arrival_delay,
             s2.department_delay as s2_department_delay
         from "Stop" s
--- app/src/app/trainStop.model.ts
+++ app/src/app/trainStop.model.ts
@@ -1,16 +1,10 @@
 export interface TrainStop {
-    station: string;
     arrival_timestamp: string;
     department_timestamp: string;
-    arrival_time: string;
-    department_time: string;
     arrival_delay: string;
     department_delay: string;
-    s2_station: string;
     s2_arrival_timestamp: string;
     s2_department_timestamp: string;
-    s2_arrival_time: string;
-    s2_department_time: string;
     s2_arrival_delay: string;
     s2_department_delay: string;
 }
