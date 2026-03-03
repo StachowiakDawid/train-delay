c0a9f40ab32ee86141035f876804f7e11528cda3
StachowiakDawid <dawidos234@gmail.com>
2026-03-03 19:22:13 +0100

    Move cancellations to another table

--- api/prisma/sql/getSimple.sql
+++ api/prisma/sql/getSimple.sql
@@ -15,9 +15,10 @@ select c.department_date + o.departments_offsets [array_position(r.array_route, 
     c.department_delays [array_position(r.array_route, $1)] as department_delay,
     c.department_date + o.arrivals_offsets [array_position(r.array_route, $2)] * interval '1 second' as s2_arrival_timestamp,
     c.arrival_delays [array_position(r.array_route, $2)] as s2_arrival_delay,
-    (c.cancelled[array_position(r.array_route, $1)] OR c.cancelled[array_position(r.array_route, $2)]) as cancelled
+    ca.id is not null and (ca.cancelled[array_position(r.array_route, $1)] OR ca.cancelled[array_position(r.array_route, $2)]) as cancelled
 from connection c
     inner join route r on r.id = c.route_id
     and r.array_route @> array[$1, $2]::varchar[] AND array_position(r.array_route, $1) < array_position(r.array_route, $2)
     inner join time_offset o on o.id = c.time_offset_id
+    left join cancellation ca on ca.id = c.cancellation_id
 order by department_timestamp desc;
