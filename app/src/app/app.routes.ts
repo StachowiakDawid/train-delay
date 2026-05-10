2079a091b4d2a3170ba2a19560c947c30c06d42d
StachowiakDawid <dawidos234@gmail.com>
2026-05-10 10:13:50 +0200

    Stay on index.html

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'index.html', component: App },
+    { path: '', redirectTo: 'index.html', pathMatch: 'full' },
+    { path: '**', redirectTo: 'index.html', pathMatch: 'full' }
+];
